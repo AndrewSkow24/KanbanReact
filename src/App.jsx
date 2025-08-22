18f87436fc93f4e44177578e4394e5e42435dda7
Andrew Skow <anjeie24@mail.ru>
2025-08-22 20:04:25 +0300

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import PopBrowse from "./components/PopBrowse/PopBrowse";
 import PopNewCard from "./components/PopNewCard/PopNewCard";
 import PopUser from "./components/PopUser/PopUser";
+import Loader from "./components/Loader/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
       <div class="wrapper">
@@ -13,7 +23,7 @@ function App() {
         <PopNewCard />
         <PopBrowse />
         <Header />
-        <Main />
+        {loading ? <Loader /> : <Main />}
       </div>
     </>
   );
